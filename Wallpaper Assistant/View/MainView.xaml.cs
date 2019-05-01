84434e69c22c7994ec422d973b52371ccb759714
GreshnikAlex <1995BLADE@mail.ru>
2019-05-01 15:34:06 +0300

    Реализовать структуру MVVM

---
+++ Wallpaper Assistant/View/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Wallpaper_Assistant
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Конструктор окна
+        /// </summary>
+        public MainWindow() => InitializeComponent();
+    }
+}
